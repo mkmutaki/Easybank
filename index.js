09f021d15701a698d2254edcc29e6ad8af58c081
mkmutaki <mkmutaki@gmail.com>
2023-08-24 16:27:22 +0300

    navbar-widget implemented ✨

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const openMenu = document.getElementById("open-button");
 const closeMenu = document.getElementById("close-button");
 const navbar = document.querySelector("nav");
+const aboutNavbar = document.querySelector(".about-navi");
+const aboutBtn = document.querySelector(".about-btn");
+const buttonUp2 = document.querySelector("#Up2");
+const buttonDown2 = document.querySelector("#Down2");
 
 openMenu.addEventListener("click", function () {
   navbar.classList.toggle("open");
+});
+
+aboutBtn.addEventListener("click", function () {
+  aboutNavbar.classList.toggle("hidden");
+  buttonDown2.classList.toggle("hidden");
+  buttonUp2.classList.toggle("hidden");
 });
